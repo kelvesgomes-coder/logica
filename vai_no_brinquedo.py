31953738db35789cd93d92ddef1b1ac530c78f1b
kelvesgomes-coder <kelves.gomes@edu.senai.br>
2026-03-23 10:04:28 -0300

    Update vai_no_brinquedo.py

--- vai_no_brinquedo.py
+++ vai_no_brinquedo.py
@@ -3,4 +3,4 @@ problema_cardiaco = "não"
 if altura > 1.5 and problema_cardiaco == "não":
     print("vai no brinquedo")
 else:
-    print("não vai no brinquedo")
+    print("não vai no brinquedo") 
